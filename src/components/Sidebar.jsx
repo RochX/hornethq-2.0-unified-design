95ef3f16ff6785201f5e75fab6f5381945f003e8
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-10-09 15:21:23 -0400

    Move sidebar CSS into its own file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import SidebarButton from "./sidebar/SidebarButton";
 
+import "./sidebar/Sidebar.css"
+
 // import link image icons
 import HomeIcon from "./sidebar/home-icon.png"
 import AcademicsIcon from "./sidebar/academics-icon.png"
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <ul>
-                <SidebarButton path="/home" src={HomeIcon} name="Home" />
-                <SidebarButton path="/academics" src={AcademicsIcon} name="Academics" />
-            </ul>
-        </div>
-    );
+  return (
+    <div className="sidebar">
+      <ul>
+        <SidebarButton path="/home" src={HomeIcon} name="Home" />
+        <SidebarButton path="/academics" src={AcademicsIcon} name="Academics" />
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
