335b03c66038061217f1d6a7650890de7bf822f4
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-10-09 16:02:38 -0400

    Move icons to the public folder

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
 import SidebarButton from "./sidebar/SidebarButton";
 
 import "./sidebar/Sidebar.css"
-
-// import link image icons
-import HomeIcon from "./sidebar/home-icon.png"
-import AcademicsIcon from "./sidebar/academics-icon.png"
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <ul>
-        <SidebarButton path="/home" src={HomeIcon} name="Home" />
-        <SidebarButton path="/academics" src={AcademicsIcon} name="Academics" />
+        {/* we assume images are in the `public` folder */}
+        <SidebarButton path="/home" src="/home-icon.png" name="Home" />
+        <SidebarButton path="/academics" src="/academics-icon.png" name="Academics" />
       </ul>
     </div>
   );
