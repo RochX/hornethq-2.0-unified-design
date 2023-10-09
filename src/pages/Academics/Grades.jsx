3401cfba96aca8769ed877017bba594773111c74
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-10-09 15:19:17 -0400

    Fix indentation within return statement

--- src/pages/Academics/Grades.jsx
+++ src/pages/Academics/Grades.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 function Grades() {
-    return (
-        <h2>This is the page for Grades</h2>
-    );
+  return (
+    <h2>This is the page for Grades</h2>
+  );
 }
 
 export default Grades;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function Home() {
-    return (
-        <div>
-            <h1>This is the Home</h1>
-            <h3>Hello Student!</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is the Home</h1>
+      <h3>Hello Student!</h3>
+    </div>
+  );
 }
 
 export default Home;
