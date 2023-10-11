fc215ef766f5714d0ae23b0d7c5a6ad5c15a7881
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-10-11 16:00:33 -0400

    Add "key" value to the PageButton map

--- src/components/PageButtonLayout.jsx
+++ src/components/PageButtonLayout.jsx
@@ -3,9 +3,12 @@ import PageButton from "./PageButton";
 
 import "./PageButtonLayout.css"
 
+import { nanoid } from 'nanoid'
+
 function PageButtonLayout (props) {
   const PAGE_BUTTONS = props.pagebuttondata.map((button) =>
     <PageButton 
+      key={`pagebutton-${nanoid()}`}
       title={button.title}
       description={button.description}
       icon={button.icon}
