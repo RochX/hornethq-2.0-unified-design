ebb727f69dee96d0431d8348490aa275bd6d785b
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-10-12 19:56:53 -0400

    Add path for the grades page into the pagebutton data

--- src/pages/Academics.jsx
+++ src/pages/Academics.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 import PageButtonLayout from "../components/PageButtonLayout"
 
 const PAGE_BUTTONS_CONTENT = [
-  {title: "Grades", description: "View grades here.", icon: ""},
+  {title: "Grades", description: "View grades here.", icon: "", path: "/academics/grades"},
   {title: "Course Catalog", description: "View the course catalog", icon: ""},
   {title: "Student Planning", description: "View and register for upcoming classes.", icon: ""},
   {title: "Unofficial Transcript", description: "Download an unofficial transcript."}
